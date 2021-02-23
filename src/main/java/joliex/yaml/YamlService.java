bf04ef9f147930242817238cd3fd8eb136972884
Mauro <msgarzi@imolainformatica.it>
2021-02-23 16:11:32 +0100

    improved version - inserted recursive method to build YAML

--- src/main/java/joliex/yaml/YamlService.java
+++ src/main/java/joliex/yaml/YamlService.java
@@ -1,5 +1,6 @@
 package joliex.yaml;
 
+import com.fasterxml.jackson.core.JsonToken;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import com.fasterxml.jackson.dataformat.yaml.YAMLParser;
 import jolie.runtime.JavaService;
@@ -9,6 +10,7 @@ import jolie.runtime.embedding.RequestResponse;
 import java.io.IOException;
 
 public class YamlService extends JavaService {
+    
     @RequestResponse
     public static Value yamlToValue(Value request) {
         Value response = Value.create();
@@ -19,12 +21,35 @@ public class YamlService extends JavaService {
 
         try {
 
-            YAMLParser parse = factory.createParser(request.getFirstChild("yaml").strValue());
-            System.out.println(parse.getCurrentName());
+            YAMLParser parser = factory.createParser(request.getFirstChild("yaml").strValue());
+            parser.nextToken();
+            buildJolieCustomData(response, parser , "");
         } catch (IOException e) {
             e.printStackTrace();
         }
         return response;
+    }
+
+    private static void buildJolieCustomData(Value response, YAMLParser parser , String nodeName) throws IOException {
+        JsonToken token = parser.nextToken();
+
+        switch (token) {
+            case FIELD_NAME:
+                System.out.println("FIELD_NAME " +parser.getCurrentName());
+                buildJolieCustomData(response, parser , parser.getCurrentName());
+                break;
+            case VALUE_STRING:
+                 System.out.println("VALUE_STRING " + parser.getValueAsString());
+                 response.getNewChild(nodeName).setValue(token.asString());
+                 break;
+            case END_ARRAY:
+                break;
+            case END_OBJECT:
+                break;
+            case START_OBJECT:
+                break;
+
+        }
     }
 
 
